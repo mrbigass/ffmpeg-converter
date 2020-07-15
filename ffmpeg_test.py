a9ddba0e9d7d904d42e7d92f9ba2728a453756e1
Hikasa Akito <58410530+mrbigass@users.noreply.github.com>
2020-07-15 17:39:50 +0900

    Update ffmpeg_test.py

--- ffmpeg_test.py
+++ ffmpeg_test.py
@@ -1,36 +1,22 @@
 # coding=UTF-8
 import pathlib
-import pdb
+import ffmpeg
 
-def main():
-    # find sub-repositories in root repository
-    base_path = pathlib.Path('Desktop')
-    # sub_dirs = base_path.iterdir()
-    sub_dirs = base_path.glob('**/familly_records')
+def main(): 
+    root_path = pathlib.Path('.')
 
-    pdb.set_trace()
+    for dir in root_path.iterdir():
+        if dir.is_file():
+            continue
 
-    for sub_dir in sub_dirs:
-        # sub_dir = ビデオ　家族の記録　No.1  1994.9.15~
+        input_file_path = dir / 'VIDEO_TS/VTS_01_1.VOB'
+        output_file_name = dir.name.replace('ビデオ　家族の記録　', '') + '.mp4'
 
-        for input_path in pathlib.Path(sub_dir).glob('**/VIDEO_TS/VTS_.*.VOB'):
-            # 入力
-            # stream = ffmpeg.input('input.VOB')
-            stream = ffmpeg.input(input_path)
+        stream = ffmpeg.input(input_file_path)
+        stream = ffmpeg.output(stream, output_file_name)
 
-            # 出力
-            output_file_name = 'output.mp4'
-            stream = ffmpeg.output(stream, output_file_name)
-
-            # 実行
-            ffmpeg.run(stream)
-
-
-
-            output_path = to_output_path(args.root_dir, input_path)
-            noop_or_create_dir(output_path)
-            convert(input_path, output_path, dry_run=args.dry_run)
-
+        ffmpeg.run(stream)
+            
 
 if __name__ == "__main__":
     main()
