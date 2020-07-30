89f8adf9a39d5471a003137fabe2c132960c5964
Hikasa Akito <58410530+mrbigass@users.noreply.github.com>
2020-07-30 10:17:49 +0900

    変換漏れと通し番号規則変更

--- ffmpeg_converter.py
+++ ffmpeg_converter.py
@@ -1,22 +1,34 @@
 # coding=UTF-8
 import pathlib
+import re
 import ffmpeg
+
 
 def main(): 
     root_path = pathlib.Path('.')
+    root_files = [x.name for x in root_path.iterdir() if x.is_file()]
 
     for dir in root_path.iterdir():
         if dir.is_file():
             continue
 
-        input_file_path = dir / 'VIDEO_TS/VTS_01_1.VOB'
-        output_file_name = dir.name.replace('ビデオ　家族の記録　', '') + '.mp4'
+        input_file_list = pathlib.Path(dir / 'VIDEO_TS/').glob('VTS_01_*.VOB')
 
-        stream = ffmpeg.input(input_file_path)
-        stream = ffmpeg.output(stream, output_file_name)
+        for input_file in input_file_list:
+            if input_file.name == 'VTS_01_0.VOB':
+                continue
+            file_num = input_file.name.replace('VTS_01_', '-').replace('.VOB', '  ')
+            output_file_name = dir.name.replace('ビデオ　家族の記録　', '').replace('  ', file_num) + '.mp4'
 
-        ffmpeg.run(stream)
-            
+            if output_file_name in root_files:
+                print('pass')
+                continue
+
+            stream = ffmpeg.input(input_file)
+            stream = ffmpeg.output(stream, output_file_name)
+
+            ffmpeg.run(stream)
+
 
 if __name__ == "__main__":
     main()
